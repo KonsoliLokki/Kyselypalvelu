9731b10d87b569e96152c2e8cdd430fff5a973db
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-04-06 16:04:34 +0300

    controller

--- src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
+++ src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
@@ -1,5 +1,14 @@
-package com.Kyselypalvelu.web;
+package com.Kyselypalvelu.webcontrol;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class QuestionController {
 
+
+	@RequestMapping(value ={"/", "/question"})
+    public String question() {	
+        return "I am question";
+    }
 }
