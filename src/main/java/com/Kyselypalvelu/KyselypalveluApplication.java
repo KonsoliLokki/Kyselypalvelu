246e74563f182dd57adc8fee90cee6c99fcba36e
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-04-08 10:17:33 +0300

    dataa

--- src/main/java/com/Kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/com/Kyselypalvelu/KyselypalveluApplication.java
@@ -1,7 +1,17 @@
 package com.Kyselypalvelu;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.Kyselypalvelu.domain.Question;
+import com.Kyselypalvelu.domain.QuestionRepository;
+import com.Kyselypalvelu.domain.Survey;
+import com.Kyselypalvelu.domain.SurveyRepository;
+
 
 @SpringBootApplication
 public class KyselypalveluApplication {
@@ -9,6 +19,19 @@ public class KyselypalveluApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KyselypalveluApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner demo(SurveyRepository srepository, QuestionRepository qrepository) {
+	return (args) -> {
+		
+	List <Question> q2 = (List<Question>) new Question();	
+	Survey s1 = new Survey ("Kysely opilaiden tietämyksestä Suomesta", 10, true, q2);
+	Question q1 = new Question("option","Who is the president of Finland?","Sauli Niinistö" ,true, true, s1);
+		
+		qrepository.save(q1);
+		srepository.save(s1);
+		
+	};
+	}
 }
 //TESTING
