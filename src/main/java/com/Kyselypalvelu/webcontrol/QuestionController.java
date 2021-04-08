4299a206174887698cd3bce4efe81cccdcf52b8b
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-04-08 09:35:33 +0300

    question rest get all

--- src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
+++ src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
@@ -1,14 +1,32 @@
 package com.Kyselypalvelu.webcontrol;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.Kyselypalvelu.domain.Question;
+import com.Kyselypalvelu.domain.QuestionRepository;
 
 @Controller
 public class QuestionController {
 
-
+@Autowired
+private QuestionRepository repos;
+	
+	
 	@RequestMapping(value ={"/", "/question"})
     public String question() {	
         return "I am question";
     }
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/questions", method = RequestMethod.GET)
+    public @ResponseBody List<Question> questionsRest() {	
+        return (List<Question>) repos.findAll();
+    } 
+	
 }
