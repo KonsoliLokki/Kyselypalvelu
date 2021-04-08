95f4d6a674cc6f270fc19e5ed7ce9cc7cb85c21d
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-08 09:34:47 +0300

    REST service for getting all questions

--- src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
+++ src/main/java/com/Kyselypalvelu/webcontrol/QuestionController.java
@@ -1,14 +1,30 @@
 package com.Kyselypalvelu.webcontrol;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.Kyselypalvelu.domain.Question;
+import com.Kyselypalvelu.domain.QuestionRepository;
 
 @Controller
 public class QuestionController {
 
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@RequestMapping(value ={"/", "/question"})
     public String question() {	
         return "I am question";
     }
+	
+	// RESTful service to get all questions
+	@GetMapping("/questions")
+	public @ResponseBody List<Question> questionsRest() {
+		return (List<Question>) questionRepository.findAll();
+	}
 }
