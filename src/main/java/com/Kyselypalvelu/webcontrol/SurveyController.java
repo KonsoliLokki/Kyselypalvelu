f7740477c4cf2e131b1810e6e6f675661f39c37b
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-08 09:50:12 +0300

    removed surveys REST from QuestionController

--- src/main/java/com/Kyselypalvelu/webcontrol/SurveyController.java
+++ src/main/java/com/Kyselypalvelu/webcontrol/SurveyController.java
@@ -1,16 +1,13 @@
 package com.Kyselypalvelu.webcontrol;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller;
-=======
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.util.List;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.Kyselypalvelu.domain.Survey;
 import com.Kyselypalvelu.domain.SurveyRepository;
