649d1a8c3bf6f8e80a11ec08d76644ae26d27fb0
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-22 10:00:05 +0300

    readded json ignore to answer class

--- src/main/java/com/Kyselypalvelu/domain/Answer.java
+++ src/main/java/com/Kyselypalvelu/domain/Answer.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Answer {
 
@@ -14,6 +16,7 @@ public class Answer {
 	private String ansText;
 	
 	@ManyToOne
+	@JsonIgnore
 	@JoinColumn(name = "questionId", referencedColumnName = "questionId")
 	private Question question;
 	
